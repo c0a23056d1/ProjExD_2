ef21138b50327423c8da25d9ed8a412c6b619392
C0A23056 <c0a23056d1@edu.teu.ac.jp>
2024-06-18 15:06:54 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,6 +16,19 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：真理値タプル（横方向、縦方向）
+    画面内ならTrue/画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:   #横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  #縦方向判定
+        tate = False
+    return yoko, tate
+
 
 
 def main():
@@ -41,23 +54,38 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        #if kk_rct.colliderect(bomb_rect):
+            #return
         screen.blit(bg_img, [0, 0]) 
 
         tmr += 1
         clock.tick(50)
 
 
-        screen.blit(bomb, bomb_rect)
-        bomb_rect.move_ip(vx, vy)
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        screen.blit(bomb, bomb_rect)
         for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        bomb_rect.move_ip(vx, vy)
+        yoko, tate = check_bound(bomb_rect)
+        if not yoko: #横方向にはみ出たら
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(kk_img, kk_rct) 
+        
+        
+
         pg.display.update()
+
+        
 
         
 
